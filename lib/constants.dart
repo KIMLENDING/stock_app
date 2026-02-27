af80aad9b8a1b5121a34654388599d98ea05bf0b
KIMLENDING <gotslaakaak@gmail.com>
2026-02-27 19:45:33 +0900

    feat:앱에서 공통으로 사용하는 상수 정의 클래스

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+/// 앱에서 공통으로 사용하는 상수 정의 클래스
+class ApiConstants {
+  // Android 에뮬레이터에서 호스트 PC 접근: 10.0.2.2
+  // Windows/웹 실행 시에는 localhost로 변경하여 사용
+  static const String baseUrl = 'http://10.0.2.2:3000';
+}
